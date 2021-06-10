c38c2c5109f542b655fabd31116a43d7c2381057
SW van Heerden <swvheerden@gmail.com>
2021-06-10 12:05:51 +0200

    Add commitment signature Co-Authored-By: Hansie Odendaal <pluto@tari.com>

--- src/commitment.rs
+++ src/commitment.rs
@@ -20,13 +20,14 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use crate::keys::PublicKey;
+use crate::keys::{PublicKey, SecretKey};
 use serde::{Deserialize, Serialize};
 use std::{
     cmp::Ordering,
     hash::{Hash, Hasher},
-    ops::{Add, Sub},
+    ops::{Add, Mul, Sub},
 };
+
 use tari_utilities::{ByteArray, ByteArrayError};
 
 /// A commitment is like a sealed envelope. You put some information inside the envelope, and then seal (commit) it.
@@ -45,10 +46,8 @@ use tari_utilities::{ByteArray, ByteArrayError};
 ///   C_2 &= v_2.G + k_2.H \\\\
 ///   \therefore C_1 + C_2 &= (v_1 + v_2)G + (k_1 + k_2)H
 /// \end{aligned} $$
-#[derive(Debug, Eq, Clone, Serialize, Deserialize, Default)]
-#[serde(bound(deserialize = "P: PublicKey"))]
-pub struct HomomorphicCommitment<P>(pub(crate) P)
-where P: PublicKey;
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct HomomorphicCommitment<P>(pub(crate) P);
 
 impl<P> HomomorphicCommitment<P>
 where P: PublicKey
@@ -117,6 +116,20 @@ where
     }
 }
 
+impl<'a, 'b, P, K> Mul<&'b K> for &'a HomomorphicCommitment<P>
+where
+    P: PublicKey<K = K>,
+    K: SecretKey,
+    &'b K: Mul<&'a P, Output = P>,
+{
+    type Output = HomomorphicCommitment<P>;
+
+    fn mul(self, rhs: &'b K) -> HomomorphicCommitment<P> {
+        let p = rhs * &self.0;
+        HomomorphicCommitment::<P>::from_public_key(&p)
+    }
+}
+
 impl<P: PublicKey> Hash for HomomorphicCommitment<P> {
     fn hash<H: Hasher>(&self, state: &mut H) {
         state.write(self.as_bytes())
@@ -129,23 +142,25 @@ impl<P: PublicKey> PartialEq for HomomorphicCommitment<P> {
     }
 }
 
+impl<P: PublicKey> Eq for HomomorphicCommitment<P> {}
+
 pub trait HomomorphicCommitmentFactory {
     type P: PublicKey;
 
-    /// Create a new commitment with the value and blinding factor provided. The implementing type will provide the
+    /// Create a new commitment with the blinding factor k and value v provided. The implementing type will provide the
     /// base values
     fn commit(&self, k: &<Self::P as PublicKey>::K, v: &<Self::P as PublicKey>::K) -> HomomorphicCommitment<Self::P>;
     /// return an identity point for addition using the specified base point. This is a commitment to zero with a zero
     /// blinding factor on the base point
     fn zero(&self) -> HomomorphicCommitment<Self::P>;
-    /// Test whether the given keys open the given commitment
+    /// Test whether the given blinding factor k and value v open the given commitment
     fn open(
         &self,
         k: &<Self::P as PublicKey>::K,
         v: &<Self::P as PublicKey>::K,
         commitment: &HomomorphicCommitment<Self::P>,
     ) -> bool;
-    /// Create a commitment from a spending key and a integer value
+    /// Create a commitment from a blinding factor k and a integer value
     fn commit_value(&self, k: &<Self::P as PublicKey>::K, value: u64) -> HomomorphicCommitment<Self::P>;
     /// Test whether the given private key and value open the given commitment
     fn open_value(&self, k: &<Self::P as PublicKey>::K, v: u64, commitment: &HomomorphicCommitment<Self::P>) -> bool;
